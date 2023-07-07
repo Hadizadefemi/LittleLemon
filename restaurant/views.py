0b60130e8dc9abd4efd35e4df3f0a1fa7b61addf
Hadiz <adefemihadizi2020@gmail.com>
2023-07-07 19:48:09 +0100

    added authentication to the API endpoints

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 def index(request):
@@ -15,8 +16,9 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
